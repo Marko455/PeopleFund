d5176cd692b8383c013173cd1303ebc55fa3051e
Marko Galavić <147241279+Marko455@users.noreply.github.com>
2025-12-07 10:18:56 +0100

    Kreiranje novog Vue3 projekta
    Ponovna incijalizacija + dodavanje metamaska zbog nedostatka typescript komponente Vue3 projekta.

--- PeopleFund/src/App.vue
+++ PeopleFund/src/App.vue
@@ -1,13 +1,14 @@
-<script setup>
+<script setup lang="ts">
 import { RouterLink, RouterView } from 'vue-router'
 import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
+    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld msg="Dobrodošli na PeopleFund!" />
+      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
